2d43245d049cf908069222d3b11078c9b4559872
Hanpoki <349391931@qq.com>
2016-05-09 17:07:48 +0800

    update

--- php.php
+++ php.php
@@ -1,40 +1,47 @@
 <?php
-$input = file_get_contents("php://input"); //接收POST数据
+$input = file_get_contents("php://input");
 $str=explode("&",$input);
 $type=explode("=",$str[0]);
 $pwd =explode("=",$str[1]);
 $name=explode("=",$str[2]);
-//var_dump($input);
 
-try {
-  $con = mysql_connect("mysql:host=localhost", 'root', 'root');
-  mysql_select_db("Userinfo",$con);
+try{
+  $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+  $pdo =new PDO('mysql:host=localhost;dbname=Userinfo','root','root',$pdo_options);
 }
-catch (Exception $e) {
-  echo json_encode(arr('code'=>'1994','value'=> 'connect failed'));
+catch(Exception $e){
+    echo("databaseerror");
 }
 
 try{
   if($type[1]=="signup"){
-    //账号与密码相同
     if($pwd[1]!=$name[1]){
-      $pdo =new PDO("mysql:host=localhost;dbname=Userinfo",'root','root');
-      if($pdo->exec("INSERT INTO userinfo (account,pwd) VALUES(".$name[1].",".$pwd[1].");")){
-           echo json_encode(arr('code'=>'200','value'=>'signup success'));
-      }
-      else
-        echo json_encode(arr('code'=>'1996','value'=> 'Not allow the same name and password'));
-    }
 
- }
- else{
-   $pdo =new PDO("mysql:host=localhost;dbname=Userinfo","root","root");
-   $result = $pdo->query("select * from userinfo where (account = ".$name[1]." and pwd = ".$pwd[1].");");
-   if($result->rowCount()!=0){
-     echo json_encode(arr('code'=>'200','value'=>'login success'));
+      if($pdo->exec("INSERT INTO userinfo (account,pwd) VALUES(".$name[1].",".$pwd[1].");")){
+           echo json_encode(array('code'=>'200','value'=>'signup success'));
+      }
+      else {
+          echo json_encode(array('code'=>'1998','value'=> 'name repeat'));
+      }
+    }
+     else{
+        echo json_encode(array('code'=>'1996','value'=> 'Not allow the same name and password'));
+    }
+  }
+  else{
+   $sql = "select * from userinfo where (account='".$name[1]."' and pwd='".$pwd[1]."');";
+   $result = $pdo->query($sql);
+   $rows = $result->fetchAll();
+   $rowCount = count($rows);
+
+   if($rowCount != 0){
+     echo json_encode(array('code'=>'200','value'=>'login success'));
+   }
+   else {
+       echo json_encode(array('code'=>'400','value'=>'pwd not match'));
    }
  }
 }catch(Exception $e){
-  echo json_encode(arr('code'=>'1998','value'=> 'name repeat'));   //账号已存在
+  print "error".$e->getMessage()."</br>";
+  die();
 }
-  mysql_close($con);
